ff37113f054d7b320a558ad12fcfc6f7bd76100c
tjsdk1260 <115769429+tjsdk1260@users.noreply.github.com>
2022-12-15 18:04:20 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+//팝업창 닫기
+let closeBtn = document.querySelector('.btn-close');
+
+//검색창
+let searchBtn = document.querySelector('.searchBtn');
+let search = document.querySelector('.search');
+let closeSearch = document.querySelectorAll('.search button');
+
+searchBtn.addEventListener('click',function(){
+    search.classList.add('on');
+});
+closeSearch[1].addEventListener('click',function(){
+    search.classList.remove('on');
+});
+//도움말 아코디언
+let help = document.querySelector('.help'),
+    accordion = document.querySelector('.accordion');
+help.addEventListener('click',function(){
+    accordion.classList.toggle('active');
+});
+
 //go top & go bottom
 let goTop = document.querySelector('.go-top');
 let goBottom = document.querySelector('.go-bottom');
