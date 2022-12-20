150a4aef8be2a56a111780f37c4a533f5b8b3f7e
tjsdk1260 <115769429+tjsdk1260@users.noreply.github.com>
2022-12-20 17:11:07 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-//팝업창 닫기
-let closePopup = document.querySelector('.btn-close');
-let popupWrap = document.querySelector('.pop-up-wrap');
-closePopup.addEventListener('click',function(){
-    popupWrap.style.display = 'none';
-});
 //스크롤 내리면 헤더 고정
 let fixed = document.querySelector('header');
 window.addEventListener('scroll',function(){
@@ -12,18 +6,27 @@ window.addEventListener('scroll',function(){
     fixed.style.position = 'fixed';
 });
 //메뉴 클릭시
-let checked = document.querySelector('.menubar');
-let gnb = document.querySelector('.gnb');
-let checkBox = document.querySelector('#check');
-checkBox.addEventListener('change',function(){
-    if(this.checked){
-        gnb.style.top = '50%';
-    }else{
-        gnb.style.top = '-300%';
-    }
-});
+// let checked = document.querySelector('.menubar');
+// let gnb = document.querySelector('.gnb');
+// let checkBox = document.querySelector('#check');
+// checkBox.addEventListener('change',function(){
+//     if(this.checked){
+//         gnb.style.top = '50%';
+//     }else{
+//         gnb.style.top = '-300%';
+//     }
+// });
 
-let makeMenu2 = ['의류','배낭','클라이밍 장비', '악세사리','활동별 분류'];
+//men&women 메뉴 클릭
+// let menBtn = document.querySelector('.men-btn');
+// let womenBtn = document.querySelector('women-btn');
+
+// menBtn.addEventListener('click',()=>{
+    
+// });
+// womenBtn.addEventListener('click',()=>{
+    
+// });
 //검색창
 let searchBtn = document.querySelector('.searchBtn');
 let search = document.querySelector('.search');
@@ -35,19 +38,19 @@ searchBtn.addEventListener('click',function(){
 closeSearch[1].addEventListener('click',function(){
     search.classList.remove('on');
 });
-//gift 스크롤 애니메이션
+// 스크롤 애니메이션
+let video = document.querySelector('.main-video');
 let giftImg = document.querySelector('.gift>img');
-console.log(giftImg);
 let giftContent = document.querySelector('.gift-content');
-
+let finderContent = document.querySelector('.finder-content');
+console.log(finderContent);
 window.addEventListener('scroll',function(){
     let value = window.scrollY;
     console.log('scrollY',value);  
-    
-    if(value==1260){
+    if(value==1400){
         giftContent.style.animation = 'slide 2s ease-out';
     }
-    else if(value>2000){
+    else if(value>1900){
         giftContent.style.animation = 'disappear 2s ease-out forwards';
     }
     else{
@@ -56,8 +59,11 @@ window.addEventListener('scroll',function(){
 });
 //도움말 아코디언
 let help = document.querySelector('.help'),
-    accordion = document.querySelector('.accordion');
+    accordion = document.querySelector('.accordion'),
+    plus = document.querySelector('.fa-plus'),
+    minus = document.querySelector('.fa-minus');
 help.addEventListener('click',function(){
+   plus.classList.toggle('active');
     accordion.classList.toggle('active');
 });
 
